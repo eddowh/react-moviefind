b6ed1e636d293d1da5e283314904f882d91a9fc6
Eddo Hintoso <eddo.hintoso@gmail.com>
2016-04-13 16:20:00 +0800

    package.json and flux + gulp + browserify boilerplate code

---
+++ src/js/utils/appAPI.js
@@ -0,0 +1,9 @@
+var AppActions = require('../actions/AppActions');
+
+// list of actions that are available
+// i.e. `searchMovies(movie)` for a movie finding app
+module.exports = {
+    searchMovies: function(movie) {
+
+    }
+}
